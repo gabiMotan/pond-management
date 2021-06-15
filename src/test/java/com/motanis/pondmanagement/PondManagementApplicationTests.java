19cd914dac0dc441c1347e9495765b774a24eee1
Gabi Motan <gabumotanu@gmail.com>
2021-06-15 13:59:19 +0300

    first commit

---
+++ src/test/java/com/motanis/pondmanagement/PondManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motanis.pondmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
